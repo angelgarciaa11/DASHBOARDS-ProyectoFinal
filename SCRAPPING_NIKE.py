fb49f2d10403ed9dd679ea4b2181159d2ac0e0d8
BRYAN19045 <bryan.cornejo@uabc.edu.mx>
2025-06-10 01:49:18 -0600

    Update SCRAPPING_NIKE.py

--- SCRAPPING_NIKE.py
+++ SCRAPPING_NIKE.py
@@ -49,6 +49,39 @@ navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()), o
 
     navegador.quit()
     return datos
+if __name__ == "__main__":
+    categorias = {
+        #hombres
+        "playeras_h": ("Hombre", "playeras", "https://www.nike.com/mx/w/hombres-playeras-y-tops-9om13znik1"),
+        "sudaderas_h": ("Hombre", "sudaderas", "https://www.nike.com/mx/w/hombres-sudaderas-con-y-sin-gorro-6riveznik1"),
+        "chamarras_h": ("Hombre", "chamarras", "https://www.nike.com/mx/w/hombres-chamarras-y-chalecos-50r7yznik1"),
+        "zapatos_h": ("Hombre", "zapatos", "https://www.nike.com/mx/w/hombres-zapatos-nik1zy7ok"),
+        "pants_h": ("Hombre", "pants", "https://www.nike.com/mx/w/hombres-pants-mallas-2kq19znik1"),
+        "shorts_h": ("Hombre", "shorts", "https://www.nike.com/mx/w/hombres-shorts-38fphznik1"),
+
+        #mujeres
+        "playeras_m": ("Mujer", "playeras", "https://www.nike.com/mx/w/mujeres-playeras-y-tops-5e1x6z9om13"),
+        "sudaderas_m": ("Mujer", "sudaderas", "https://www.nike.com/mx/w/mujeres-sudaderas-con-y-sin-gorro-6rivez5e1x6"),
+        "chamarras_m": ("Mujer", "chamarras", "https://www.nike.com/mx/w/mujeres-chamarras-y-chalecos-50r7yz5e1x6"),
+        "zapatos_m": ("Mujer", "zapatos", "https://www.nike.com/mx/w/mujeres-zapatos-5e1x6zy7ok"),
+        "pants_m": ("Mujer", "pants", "https://www.nike.com/mx/w/mujeres-pants-mallas-5e1x6z6tpi6"),
+        "shorts_m": ("Mujer", "shorts", "https://www.nike.com/mx/w/mujeres-shorts-5e1x6z6ymx6"),
+
+        #unisex(articulos)
+        "mochilas": ("Unisex", "mochilas", "https://www.nike.com/mx/w/mochilas-y-bolsas-9xy71znik1")
+    }
+
+    total = []
+
+    for clave, (gen, cat, link) in categorias.items():
+        productos = scrapear_rapido(gen, cat, link, paginas=6)
+        total.extend(productos)
+
+    df = pd.DataFrame(total)
+    df.to_csv("nike_datos_rapido_mas.csv", index=False)
+    print(f"Total productos extraídos: {len(df)}")
+
+
 
     
  
